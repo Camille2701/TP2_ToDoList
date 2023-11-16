2ce62274087996c961bb7def8b8dc53afc7bd3ea
Camille <veillardcamille01@gmail.com>
2023-11-16 11:43:09 +0100

    ajout fonction

--- liste.c
+++ liste.c
@@ -9,3 +9,29 @@ TaskList* create_empty_task_list() {
     list->head = NULL;
     return list;
 }
+
+Task* create_task(char* nom, int jours) {
+    Task* new_task = (Task*)malloc(sizeof(Task));
+    if (new_task == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    new_task->nom = nom;
+    new_task->date_creation = time(NULL);
+    new_task->statut = EN_ATTENTE;
+    new_task->jours_restants = jours;
+    new_task->next = NULL;
+    return new_task;
+}
+
+void add_task(TaskList* list, Task* task) {
+    if (list->head == NULL) {
+        list->head = task;
+    } else {
+        Task* current = list->head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = task;
+    }
+}
