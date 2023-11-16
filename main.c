4408bb79504b93dab4ad4886ca193877a93fdf8e
Camille <veillardcamille01@gmail.com>
2023-11-16 12:50:04 +0100

    rien

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 
 
 int main() {
+    
     TaskList* list = create_empty_task_list();
     add_task(list, create_task("Task 1", 5));
     add_task(list, create_task("Task 2", 3));
 
     print_tasks_in_order(list);
-    
+
 
 return 0;
 }
