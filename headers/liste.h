2d90aa3655eee4945cf5c7445eeb25e4b4494c6f
Camille <veillardcamille01@gmail.com>
2023-11-16 10:29:23 +0100

    Ajout des fichiers principaux liste et main .c

---
+++ headers/liste.h
@@ -0,0 +1,16 @@
+// Created by Camille VEILLARD on 27/10/2023
+
+#ifndef liste_H
+#define liste_H
+
+//Insérer les bibliothèques nécessaires
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Insérer les nouveaux types
+
+
+//Insérer les autres prototypes ici 
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "headers/liste.h"
+
+
+int main() {
+    
+    
+    return 0;
+}
+
