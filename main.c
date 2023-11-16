aa233cd6e84d7404e0b6a219a4d62aef9c725c5a
Camille <veillardcamille01@gmail.com>
2023-11-16 12:49:15 +0100

    modification du fichier main.c

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "headers/liste.h"
 
 
+#include "headers/liste.h"
+
+
 int main() {
+    TaskList* list = create_empty_task_list();
+    add_task(list, create_task("Task 1", 5));
+    add_task(list, create_task("Task 2", 3));
+
+    print_tasks_in_order(list);
     
-    
-    return 0;
+
+return 0;
 }
 
